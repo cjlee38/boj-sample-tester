2b1ac809612b55ad1203ae60c3453e6a75b4b066
cjlee38 <cjlee38@github.com>
2023-01-07 23:29:24 +0900

    chore: gitignore hidden files

--- plugin/src/main/kotlin/io/github/cjlee38/plugin/ExecutionAction.kt
+++ plugin/src/main/kotlin/io/github/cjlee38/plugin/ExecutionAction.kt
@@ -2,9 +2,15 @@ package io.github.cjlee38.plugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys.EDITOR
+import com.intellij.openapi.actionSystem.PlatformCoreDataKeys.VIRTUAL_FILE
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.VirtualFile
 
 class ExecutionAction : AnAction() {
     override fun actionPerformed(actionEvent: AnActionEvent) {
+        val file = actionEvent.getData(VIRTUAL_FILE) ?: return
+        val name = file.name
     }
 }
 
