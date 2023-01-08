91a5f96f5e831be66ab6a81ab0d910e23e2f753b
cjlee38 <cjlee38@github.com>
2023-01-08 18:23:17 +0900

    refactor: determine executor with file extension

--- core/src/main/kotlin/io/github/cjlee38/bojsampletester/SampleTester.kt
+++ core/src/main/kotlin/io/github/cjlee38/bojsampletester/SampleTester.kt
@@ -1,5 +1,7 @@
 package io.github.cjlee38.bojsampletester
 
+import io.github.cjlee38.bojsampletester.executor.Executor
+import io.github.cjlee38.bojsampletester.executor.JavaExecutor
 import io.github.cjlee38.bojsampletester.executor.PythonExecutor
 import io.github.cjlee38.bojsampletester.request.JsoupRequestClient
 
@@ -7,8 +9,19 @@ class SampleTester {
     fun run(solution: Solution, problemNumber: String): Grades {
         val client = JsoupRequestClient()
         val problem = client.request(problemNumber)
-        val executor = PythonExecutor(solution) // todo: determine executor
+
+        val executor = determineExecutor(solution)
         val grades = executor.execute(problem)
         return grades
+    }
+
+    private fun determineExecutor(solution: Solution): Executor {
+        if (solution.path.endsWith(".java")) {
+            return JavaExecutor(solution)
+        }
+        if (solution.path.endsWith(".py")) {
+            return PythonExecutor(solution)
+        }
+        throw IllegalArgumentException("undefined extension for executor : ${solution.path}")
     }
 }
