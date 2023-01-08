d2060dc5efb82e334fc9187b9fe8728d0a562185
cjlee38 <cjlee38@github.com>
2023-01-08 20:01:22 +0900

    feat: run test from plugin action

--- plugin/src/main/kotlin/io/github/cjlee38/plugin/ExecutionAction.kt
+++ plugin/src/main/kotlin/io/github/cjlee38/plugin/ExecutionAction.kt
@@ -4,13 +4,27 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys.EDITOR
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys.VIRTUAL_FILE
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.ui.Messages
+import io.github.cjlee38.bojsampletester.SampleTester
+import io.github.cjlee38.bojsampletester.Solution
 
 class ExecutionAction : AnAction() {
     override fun actionPerformed(actionEvent: AnActionEvent) {
+        val problemNumber: String = inputProblemNumber()
+
         val file = actionEvent.getData(VIRTUAL_FILE) ?: return
-        val name = file.name
+        val path: String = file.canonicalPath ?: return
+
+        val editor = actionEvent.getData(EDITOR) ?: return
+        val text = editor.document.text
+
+        val solution = Solution(path, text)
+        val grades = SampleTester().run(solution, problemNumber)
+        println("grades = ${grades}")
     }
+
+    private fun inputProblemNumber() =
+        Messages.showInputDialog("input problem number", "boj-sample-tester", Messages.getQuestionIcon())
+            ?: throw IllegalArgumentException("problem number should not be null")
 }
 
