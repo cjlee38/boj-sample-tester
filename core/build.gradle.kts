8d306e39fe25dd3c98dc12c168ad767fe8783266
cjlee38 <cjlee38@github.com>
2023-01-09 21:09:31 +0900

    chore: add logger

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.15.3")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    implementation ("io.github.microutils:kotlin-logging-jvm:2.0.11")
 }
 
 tasks.test {
--- core/src/main/kotlin/io/github/cjlee38/bojsampletester/request/JsoupRequestClient.kt
+++ core/src/main/kotlin/io/github/cjlee38/bojsampletester/request/JsoupRequestClient.kt
@@ -3,12 +3,15 @@ package io.github.cjlee38.bojsampletester.request
 import io.github.cjlee38.bojsampletester.data.Problem
 import io.github.cjlee38.bojsampletester.data.Sample
 import io.github.cjlee38.bojsampletester.utils.toMillis
+import mu.KotlinLogging
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 
+private val log = KotlinLogging.logger {}
 
 class JsoupRequestClient : RequestClient {
     override fun request(problemNumber: String): Problem {
+        log.trace { "Requested problem Number : $problemNumber" }
         val document = Jsoup.connect(BASE_URL + problemNumber).userAgent(HEADER_USER_AGENT).get()
         val timeElement = (document.selectFirst("#problem-info > tbody > tr > td:nth-child(1)")
             ?: throw IllegalArgumentException("fails on find time"))
